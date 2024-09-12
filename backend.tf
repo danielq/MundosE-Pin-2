9529e93aa63bdcd88b75c5749c4575a95640419c
danielq <djuanrique@gmail.com>
2024-09-12 18:51:38 -0500

    renombrar archivo backend.tif

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "pin2"
+    bucket                 = "pin2-prueba"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
